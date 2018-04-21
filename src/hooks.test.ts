7e454d48c5d3a516149b67bbcdf11b7b528e3872
linyows <linyows@gmail.com>
2018-04-22 05:42:10 +0900

    rename test

--- src/hooks.test.ts
+++ src/hooks.test.ts
@@ -1,6 +1,6 @@
 import * as TD from 'testdouble'
-import test from 'ava'
-import * as Vault from './sequelize-vault'
+import Test from 'ava'
+import * as Vault from './hooks'
 const Sequelize = require('sequelize')
 
 const sequelize =  new Sequelize({
@@ -38,37 +38,27 @@ sequelize['queryInterface'].createTable('users', schema)
 Vault.shield(User)
 TD.replace(process.stdout, 'write')
 
-test('replace vault attributes "before save" to database', async (t) => {
+Test('replace vault attributes "before save" to database', async (t) => {
   const u = User.build({name: 'foobar', email: 'foo@example.com'})
   await u.save()
   t.is(u.email_encrypted, 'A2BPy5oy0zYg1iG5wuGqzg==')
   t.is(u.email, 'foo@example.com')
 })
 
-test('replace vault attributes "before create" to database', async (t) => {
+Test('replace vault attributes "before create" to database', async (t) => {
   const u = await User.create({name: 'foobar', email: 'foo@example.com'})
   t.is(u.email_encrypted, 'A2BPy5oy0zYg1iG5wuGqzg==')
   t.is(u.email, 'foo@example.com')
 })
 
-test('set vault attributes "after find" to database', async (t) => {
+Test('set vault attributes "after find" to database', async (t) => {
   await User.create({name: 'foobaz', email: 'foo@example.com'})
   const u = await User.findOne({where: { name: 'foobaz' }})
   t.is(u.email, 'foo@example.com')
 })
 
-test('set vault attributes "before find" to database', async (t) => {
+Test('set vault attributes "before find" to database', async (t) => {
   await User.create({name: 'foobaz', email: 'foo@example.com'})
   const u = await User.findOne({where: { email: 'foo@example.com' }})
   t.is(u.email, 'foo@example.com')
-})
-
-test('buildPath returns path', async (t) => {
-  const p = Vault.buildPath('users', 'name')
-  t.is(p, 'my-app_users_name')
-})
-
-test('memoryForKey returns path', async (t) => {
-  const m = Vault.memoryForKey('foo', 'bar')
-  t.is(m, 'Zm9vL2Jhcg==')
 })
