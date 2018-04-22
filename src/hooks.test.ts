b20c29258ff0da198302b76450b21006fb6c5a43
linyows <linyows@gmail.com>
2018-04-22 14:28:17 +0900

    add integration to test message

--- src/hooks.test.ts
+++ src/hooks.test.ts
@@ -38,26 +38,26 @@ sequelize['queryInterface'].createTable('users', schema)
 Vault.shield(User)
 TD.replace(process.stdout, 'write')
 
-Test('replace vault attributes "before save" to database', async (t) => {
+Test('(integration) replace vault attributes "before save" to database', async (t) => {
   const u = User.build({name: 'foobar', email: 'foo@example.com'})
   await u.save()
   t.is(u.email_encrypted, 'A2BPy5oy0zYg1iG5wuGqzg==')
   t.is(u.email, 'foo@example.com')
 })
 
-Test('replace vault attributes "before create" to database', async (t) => {
+Test('(integration) replace vault attributes "before create" to database', async (t) => {
   const u = await User.create({name: 'foobar', email: 'foo@example.com'})
   t.is(u.email_encrypted, 'A2BPy5oy0zYg1iG5wuGqzg==')
   t.is(u.email, 'foo@example.com')
 })
 
-Test('set vault attributes "after find" to database', async (t) => {
+Test('(integration) set vault attributes "after find" to database', async (t) => {
   await User.create({name: 'foobaz', email: 'foo@example.com'})
   const u = await User.findOne({where: { name: 'foobaz' }})
   t.is(u.email, 'foo@example.com')
 })
 
-Test('set vault attributes "before find" to database', async (t) => {
+Test('(integration) set vault attributes "before find" to database', async (t) => {
   await User.create({name: 'foobaz', email: 'foo@example.com'})
   const u = await User.findOne({where: { email: 'foo@example.com' }})
   t.is(u.email, 'foo@example.com')
