72aca2ea1b567e57d3ee3e740df27dc891e9beb4
linyows <linyows@gmail.com>
2018-05-10 23:40:47 +0900

    add hook for after-save

--- src/hooks.ts
+++ src/hooks.ts
@@ -21,6 +21,8 @@ export function addHooks(model: any) {
   model.afterFind('loadAttributesOnAfterFind', loadAttributesOnAfterFind)
   model.beforeCreate('persistAttributesOnBeforeSave', persistAttributesOnBeforeSave)
   model.beforeUpdate('persistAttributesOnBeforeSave', persistAttributesOnBeforeSave)
+  model.afterCreate('persistAttributesOnAfterSave', persistAttributesOnAfterSave)
+  model.afterUpdate('persistAttributesOnAfterSave', persistAttributesOnAfterSave)
 }
 
 async function loadAttributesOnBeforeFind(query: any): Promise<void> {
@@ -86,6 +88,31 @@ async function persistAttributesOnBeforeSave(ins: any, opts: Object, fn?: Functi
       const key = Vault.BUILD_PATH(table, replaced)
       const ciphertext = await vault.encrypt(key, plaintext)
       ins.setDataValue(f, ciphertext)
+    }
+  }
+
+  return fn !== undefined ? fn(null, ins) : ins
+}
+
+async function persistAttributesOnAfterSave(ins: any, opts: Object, fn?: Function | undefined): Promise<void> {
+  if (opts['fields'] !== undefined) {
+    const vault = new Vault()
+    const rawAttrs = ins.constructor.prototype.rawAttributes
+    const table = ins.constructor.tableName
+
+    for (const f of opts['fields']) {
+      const field = rawAttrs === undefined ? f : rawAttrs[f]['field']
+      const replaced = field.replace(Vault.suffix, '')
+      if (replaced === field) {
+        continue
+      }
+      const ciphertext = ins.getDataValue(f)
+      if (!ciphertext || ciphertext === '') {
+        continue
+      }
+      const key = Vault.BUILD_PATH(table, replaced)
+      const plaintext = await vault.decrypt(key, ciphertext)
+      ins.setDataValue(replaced, plaintext)
     }
   }
 
