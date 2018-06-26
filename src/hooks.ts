84f58edbb44631963c2c3dbc431d448b4594f312
linyows <linyows@gmail.com>
2018-06-26 23:04:31 +0900

    fix fields

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,20 +1,15 @@
 import {Vault} from './vault'
 
-const fields: string[] = []
+const fields: object = {}
 let tableName: string = ''
 
 export function addHooks(model: any) {
   tableName = model.tableName
-  const arrayAttrs = model.prototype.attributes
+  fields[tableName] = {}
   const rawAttrs = model.rawAttributes
 
-  for (const attr of arrayAttrs) {
-    const field = rawAttrs === undefined ? attr : rawAttrs[attr]['field']
-    const replaced = field.replace(Vault.suffix, '')
-    if (replaced === field) {
-      continue
-    }
-    fields.push(replaced)
+  for (const key of Object.keys(rawAttrs)) {
+    fields[tableName][rawAttrs[key]['field']] = rawAttrs[key]['fieldName']
   }
 
   model.beforeFind('loadAttributesOnBeforeFind', loadAttributesOnBeforeFind)
@@ -32,10 +27,11 @@ async function loadAttributesOnBeforeFind(query: any): Promise<void> {
 
   const vault = new Vault()
 
-  for (const f of fields) {
-    if (query.where[f] !== undefined && typeof query.where[f] === 'string') {
+  for (const f of Object.keys(fields[tableName])) {
+    const encryptedFieldName = `${f}${Vault.suffix}`
+    if (query.where[f] !== undefined && typeof query.where[f] === 'string' && fields[tableName][encryptedFieldName] !== undefined) {
       const key = Vault.BUILD_PATH(tableName, f)
-      query.where[`${f}${Vault.suffix}`] = await vault.encrypt(key, query.where[f])
+      query.where[encryptedFieldName] = await vault.encrypt(key, query.where[f])
       delete query.where[f]
     }
   }
@@ -80,15 +76,10 @@ async function persistAttributesOnBeforeSave(ins: any, opts: Object, fn?: Functi
     const rawAttrs = ins.constructor.prototype.rawAttributes
     const table = ins.constructor.tableName
 
-    let fields: object = {}
-    for (const key of Object.keys(rawAttrs)) {
-      fields[rawAttrs[key]['field']] = rawAttrs[key]['fieldName']
-    }
-
     for (const f of opts['fields']) {
       const field = rawAttrs === undefined ? f : rawAttrs[f]['field']
       const encryptedFieldName = field+Vault.suffix
-      if (fields[encryptedFieldName] === undefined) {
+      if (fields[table][encryptedFieldName] === undefined) {
         continue
       }
       const plaintext = ins.getDataValue(field)
@@ -97,7 +88,7 @@ async function persistAttributesOnBeforeSave(ins: any, opts: Object, fn?: Functi
       }
       const key = Vault.BUILD_PATH(table, field)
       const ciphertext = await vault.encrypt(key, plaintext)
-      ins.setDataValue(fields[encryptedFieldName], ciphertext)
+      ins.setDataValue(fields[table][encryptedFieldName], ciphertext)
     }
   }
 
