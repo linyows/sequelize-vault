b56c8891fa2e755d50c393a04844d3994138540f
linyows <linyows@gmail.com>
2018-11-02 01:55:16 +0900

    fix test

--- src/hooks.test.ts
+++ src/hooks.test.ts
@@ -13,6 +13,18 @@ const sequelize =  new Sequelize({
   operatorsAliases: false,
 })
 
+const randomNumber = (max: number) => {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const genName = () => {
+  return `foobar-${randomNumber(100000)}`
+}
+
+const genEmail = () => {
+  return `${randomNumber(100000)}@example.com`
+}
+
 const schema = {
   id: {
     type: Sequelize.INTEGER,
@@ -21,6 +33,7 @@ const schema = {
   },
   name: Sequelize.STRING,
   email_encrypted: Sequelize.STRING,
+  credit_card_number_encrypted: Sequelize.STRING,
   created_at: Sequelize.DATE,
   updated_at: Sequelize.DATE,
 }
@@ -29,6 +42,8 @@ const User = sequelize.define('user', {
   name: Sequelize.STRING,
   email_encrypted: Sequelize.STRING,
   email: Sequelize.VIRTUAL,
+  credit_card_number_encrypted: Sequelize.STRING,
+  credit_card_number: Sequelize.VIRTUAL,
 },
 {
   tableName: 'users',
@@ -42,43 +57,58 @@ addHooks(User)
 TD.replace(process.stdout, 'write')
 
 Test('when native, replace vault attributes "before save" to database', async (t) => {
-  const u = User.build({name: 'foobar', email: 'foo@example.com'})
+  const name = genName()
+  const u = User.build({name, email: 'foo@example.com', credit_card_number: '0000111122223333'})
   await u.save()
   t.is(u.email_encrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(u.email, 'foo@example.com')
+  t.is(u.credit_card_number_encrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(u.credit_card_number, '0000111122223333')
 })
 
 Test('when native, replace vault attributes "before create" to database', async (t) => {
-  const u = await User.create({name: 'foobar', email: 'foo@example.com'})
+  const name = genName()
+  const u = await User.create({name, email: 'foo@example.com', credit_card_number: '0000111122223333'})
   t.is(u.email_encrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(u.email, 'foo@example.com')
+  t.is(u.credit_card_number_encrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(u.credit_card_number, '0000111122223333')
 })
 
 Test('when native, replace vault attributes "before update" to database', async (t) => {
-  const u = await User.create({name: 'foobar', email: 'foo@example.com'})
+  const name = genName()
+  const u = await User.create({name, email: 'foo@example.com', credit_card_number: '0000111122223333'})
   u.email = 'foo-to-zoo@example.com'
   await u.save()
   t.not(u.email_encrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(u.email, 'foo-to-zoo@example.com')
+  t.is(u.credit_card_number_encrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(u.credit_card_number, '0000111122223333')
 })
 
 Test('when native, set vault attributes "after find" to database', async (t) => {
-  await User.create({name: 'foobaz', email: 'foo@example.com'})
-  const u = await User.findOne({where: { name: 'foobaz' }})
+  const name = genName()
+  await User.create({name, email: 'foo@example.com', credit_card_number: '0000111122223333'})
+  const u = await User.findOne({where: { name }})
   t.is(u.email, 'foo@example.com')
+  t.is(u.credit_card_number, '0000111122223333')
 })
 
 Test('when native, use findOneByEncrypted', async (t) => {
-  await User.create({name: 'foobaz', email: 'foo@example.com'})
-  const u = await findOneByEncrypted(User, { email: 'foo@example.com' }) as any
-  t.is(u.email, 'foo@example.com')
+  const name = genName()
+  const email = genEmail()
+  await User.create({name, email, credit_card_number: '0000111122223333'})
+  const u = await findOneByEncrypted(User, { email }) as any
+  t.is(u.email, email)
+  t.is(u.credit_card_number, '0000111122223333')
 })
 
 Test('when native, skip decrypt on "find all"', async (t) => {
-  await User.create({name: 'findall', email: 'findall@example.com'})
+  await User.create({name: 'findall', email: 'findall@example.com', credit_card_number: '0000111122223333'})
   const users = await User.findAll()
   t.is(users[users.length-1].name, 'findall')
   t.is(users[users.length-1].email, undefined)
+  t.is(users[users.length-1].credit_card_number, undefined)
 })
 
 const sequelizeTS = new SequelizeTS({
@@ -101,11 +131,11 @@ class Person extends Model<Person> {
   @Column({ field: 'email_encrypted' })
   public emailEncrypted: string
 
-  @Column({ field: 'snake_cased_secret_value', type: DataType.VIRTUAL })
-  public snakeCasedSecretValue: string
+  @Column({ field: 'credit_card_number', type: DataType.VIRTUAL })
+  public creditCardNumber: string
 
-  @Column({ field: 'snake_cased_secret_value_encrypted' })
-  public snakeCasedSecretValueEncrypted: string
+  @Column({ field: 'credit_card_number_encrypted' })
+  public creditCardNumberEncrypted: string
 }
 
 const schemaTS = {
@@ -116,7 +146,7 @@ const schemaTS = {
   },
   name: DataType.STRING,
   email_encrypted: DataType.STRING,
-  snake_cased_secret_value_encrypted: DataType.STRING,
+  credit_card_number_encrypted: DataType.STRING,
 }
 
 sequelizeTS['queryInterface'].createTable('persons', schemaTS)
@@ -124,57 +154,64 @@ sequelizeTS.addModels([Person])
 addHooks(Person)
 
 Test('when typescript, replace vault attributes "before save" to database', async (t) => {
-  const p = Person.build({name: 'foobar', email: 'foo@example.com', snakeCasedSecretValue: 'my secret value'})
+  const name = genName()
+  const p = Person.build({name, email: 'foo@example.com', creditCardNumber: '0000111122223333'})
   await p.save()
   t.is(p.emailEncrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(p.email, 'foo@example.com')
-  t.is(p.snakeCasedSecretValueEncrypted, 'W7IqykOlzYx+fGbjCxaWxw==')
-  t.is(p.snakeCasedSecretValue, 'my secret value')
+  t.is(p.creditCardNumberEncrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(p.creditCardNumber, '0000111122223333')
 })
 
 Test('when typescript, replace vault attributes "before create" to database', async (t) => {
-  const p = await Person.create({name: 'foobar', email: 'foo@example.com', snakeCasedSecretValue: 'my secret value'})
+  const name = genName()
+  const p = await Person.create({name, email: 'foo@example.com', creditCardNumber: '0000111122223333'})
   t.is(p.emailEncrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(p.email, 'foo@example.com')
-  t.is(p.snakeCasedSecretValueEncrypted, 'W7IqykOlzYx+fGbjCxaWxw==')
-  t.is(p.snakeCasedSecretValue, 'my secret value')
+  t.is(p.creditCardNumberEncrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(p.creditCardNumber, '0000111122223333')
 })
 
 Test('when typescript, replace vault attributes "before update" to database', async (t) => {
-  const p = await Person.create({name: 'foobar', email: 'foo@example.com', snakeCasedSecretValue: 'my secret value'})
+  const name = genName()
+  const p = await Person.create({name, email: 'foo@example.com', creditCardNumber: '0000111122223333'})
   p.email = 'foo-to-zoo@example.com'
   await p.save()
   t.not(p.emailEncrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(p.email, 'foo-to-zoo@example.com')
-  t.is(p.snakeCasedSecretValueEncrypted, 'W7IqykOlzYx+fGbjCxaWxw==')
-  t.is(p.snakeCasedSecretValue, 'my secret value')
+  t.is(p.creditCardNumberEncrypted, 'KMT0s+O8EqtiezZo6xQbIGkZuRbEBM04hKxuDqQaNeA=')
+  t.is(p.creditCardNumber, '0000111122223333')
 })
 
 Test('when typescript, set vault attributes "after find" to database', async (t) => {
-  await Person.create({name: 'foobaz', email: 'foo@example.com', snakeCasedSecretValue: 'my secret secret value'})
-  const p = await Person.findOne<Person>({where: { name: 'foobaz' }})
+  const name = genName()
+  await Person.create({name, email: 'foo@example.com', creditCardNumber: '0000111122223333'})
+  const p = await Person.findOne<Person>({where: { name }})
   if (p === null) {
     return
   }
   t.is(p.email, 'foo@example.com')
-  t.is(p.snakeCasedSecretValue, 'my secret secret value')
+  t.is(p.creditCardNumber, '0000111122223333')
 })
 
 Test('when typescript, use findOneByEncrypted', async (t) => {
-  await Person.create({name: 'foobaz', email: 'baz@example.com', snakeCasedSecretValue: 'my secret secret secret value'})
-  const p = await findOneByEncrypted<Person>(Person, { email: 'baz@example.com' })
+  const name = genName()
+  const email = genEmail()
+  await Person.create({name, email, creditCardNumber: '0000111122223333'})
+  const p = await findOneByEncrypted<Person>(Person, { email })
   if (p === null) {
     return
   }
-  t.is(p.email, 'baz@example.com')
-  t.is(p.snakeCasedSecretValue, 'my secret secret secret value')
+  t.is(p.email, email)
+  t.is(p.creditCardNumber, '0000111122223333')
 })
 
 Test('when typescript, skip decrypt on "find all"', async (t) => {
-  await Person.create({name: 'findall', email: 'findall@example.com', snakeCasedSecretValue: 'my secret value'})
+  await Person.create({name: 'findall', email: 'findall@example.com', creditCardNumber: '0000111122223333'})
   const persons = await Person.findAll<Person>()
   t.is(persons[persons.length-1].name, 'findall')
   t.is(persons[persons.length-1].email, undefined)
+  t.is(persons[persons.length-1].creditCardNumber, undefined)
 })
 
 Test('when typescript and postgres, replace vault attributes "before save" to database', async (t) => {
@@ -188,6 +225,12 @@ Test('when typescript and postgres, replace vault attributes "before save" to da
 
     @Column({ field: 'email_encrypted' })
     public emailEncrypted: string
+
+    @Column({ field: 'credit_card_number', type: DataType.VIRTUAL })
+    public creditCardNumber: string
+
+    @Column({ field: 'credit_card_number_encrypted' })
+    public creditCardNumberEncrypted: string
   }
 
   const sequelize = new SequelizeTS({
@@ -205,16 +248,19 @@ Test('when typescript and postgres, replace vault attributes "before save" to da
       autoIncrement: true,
     },
     name: DataType.STRING,
+    email_encrypted: DataType.STRING,
+    credit_card_number_encrypted: DataType.STRING,
     // If the column of virtual type exists, its value is returned as null.
     email: DataType.STRING,
-    email_encrypted: DataType.STRING,
+    credit_card_number: DataType.STRING,
   }
 
   await sequelize['queryInterface'].createTable('admins', schema)
   sequelize.addModels([Admin])
   addHooks(Admin)
 
-  const a = Admin.build({name: 'foobar', email: 'foo@example.com'})
+  const name = genName()
+  const a = Admin.build({name, email: 'foo@example.com', creditCardNumber: '0000111122223333'})
   await a.save()
   t.is(a.emailEncrypted, 'DXFOoiyZq30TEwAu+8tFoQ==')
   t.is(a.email, 'foo@example.com')
